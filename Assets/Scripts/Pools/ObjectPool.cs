84ec90d304f920df5fc7c2dae7d6354aa8c1c58f
Sina Taheri <79784688+sinatb@users.noreply.github.com>
2025-01-15 17:25:06 +0330

    feat : created object pool

---
+++ Assets/Scripts/Pools/ObjectPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Pools
+{
+    [System.Serializable]
+    public class StringObjectPair
+    {
+        public string key;
+        public GameObject obj;
+    }
+    public class ObjectPool : MonoBehaviour
+    {
+        public List<StringObjectPair>               prefabs;
+        public int                                  count;
+        private Dictionary<string,List<GameObject>> _pool;
+
+        private void Awake()
+        {
+            _pool = new Dictionary<string, List<GameObject>>();
+            foreach (var pair in prefabs)
+            {
+                var l = new List<GameObject>();
+                for (var i = 0; i < count; i++)
+                {
+                    var o = Instantiate(pair.obj);
+                    o.SetActive(false);
+                    l.Add(o);
+                }
+                _pool.Add(pair.key, l);
+            }
+        }
+        public GameObject GetPooledObject(string key)
+        {
+            var l = _pool[key];
+            if (l == null)
+            {
+                return null;
+            }
+            foreach (var o in l)
+            {
+                if (!o.activeInHierarchy)
+                {
+                    return o;
+                }
+            }
+            return null;
+        }
+    }
+}
