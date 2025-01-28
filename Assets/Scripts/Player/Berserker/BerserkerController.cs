14ccd29ca1e99f2b6e54561911bc02462a8a8c13
Sina Taheri <79784688+sinatb@users.noreply.github.com>
2025-01-28 20:35:49 +0330

    feat : added controls to the berserker class

--- Assets/Scripts/Player/Berserker/BerserkerController.cs
+++ Assets/Scripts/Player/Berserker/BerserkerController.cs
@@ -1,4 +1,6 @@
 ﻿using Combat;
+using Enemies;
+using Managers;
 using UnityEngine;
 
 namespace Player.Berserker
@@ -10,6 +12,26 @@ namespace Player.Berserker
             var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mouseWorldPos.z = 0f;
             var dir = (mouseWorldPos - transform.position).normalized;
+            var hits = Physics2D.OverlapCircleAll(transform.position, 2.5f);
+
+            foreach (Collider2D hit in hits)
+            {
+                var directionToTarget = 
+                    ((Vector2)hit.transform.position - (Vector2)transform.position).normalized;
+                
+                var angle = Vector2.Angle(dir, directionToTarget);
+
+                if (angle <= 40.0f && hit.CompareTag("Enemy")) 
+                {
+                    var enemy = hit.gameObject.GetComponent<BaseEnemy>();
+                    if (data.Effect != null)
+                    {
+                        enemy.AddEffect(data.Effect);
+                    }
+                    enemy.TakeDamage(data.Damage);
+                    GameManager.Instance.player.Heal(data.Damage*data.LifeSteal);
+                }
+            }
         }
 
         public override void Special()
